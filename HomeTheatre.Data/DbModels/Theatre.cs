5248717ebdbdd358fa26c8801320ca693f270870
Martu <martinpismymail@gmail.com>
2019-12-10 20:30:52 +0200

    Added ViewModels and Mappers for the Theatre and User classes

--- HomeTheatre.Data/DbModels/Theatre.cs
+++ HomeTheatre.Data/DbModels/Theatre.cs
@@ -1,6 +1,8 @@
 ﻿using HomeTheatre.Data.Utilities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HomeTheatre.Data.DbModels
@@ -8,8 +10,15 @@ namespace HomeTheatre.Data.DbModels
     public class Theatre : IEntityId,IDeletable,IAuditable
     {
         public Guid Id { get; set; }
+
+        [DisplayName("Theatre Name")]
+        [Required]
+        [StringLength(40, ErrorMessage = "The  value cannot exceed 40 characters.")]
         public string Name { get; set; }
 
+        [DisplayName("Theatre AboutInfo")]
+        [Required]
+        [StringLength(1000, ErrorMessage = "The value cannot exceed 1000 characters.")]
         public string AboutInfo { get; set; }
 
         public string Location { get; set; }
