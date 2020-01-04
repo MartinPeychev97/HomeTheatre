f6768d4eb65da817b8acae3e23b932db95f10e30
Martu <martinpismymail@gmail.com>
2020-01-04 10:27:26 +0200

    Added Http Exeptin handlers for status code 500 and 404

---
+++ HomeTheatre.Test/ServicesTests/CommentTests/CreateCommentAsync_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTheatre.Test.ServicesTests.CommentTests
+{
+    public class CreateCommentAsync_Should
+    {
+    }
+}
